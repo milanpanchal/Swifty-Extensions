173d9aecfd57f62bf7f68c4b309e6424d972edea
Milan Panchal <sam07it22@gmail.com>
2020-10-29 13:19:06 +0530

    Get instance of appDelegate, sceneDelegate and window

--- Swifty-Extensions/Extensions/UIKit/UIViewController+Extension.swift
+++ Swifty-Extensions/Extensions/UIKit/UIViewController+Extension.swift
@@ -10,8 +10,28 @@ import UIKit
 
 extension UIViewController {
     
+    /// Get instance of the the AppDelegate
     var appDelegate: AppDelegate {
         return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    /// Get instance of the the SceneDelegate, for iOS 13+
+    var sceneDelegate: SceneDelegate? {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let delegate = windowScene.delegate as? SceneDelegate else { return nil }
+         return delegate
+    }
+
+    /// Get the instance of the window
+    var window: UIWindow? {
+        if #available(iOS 13, *) {
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let delegate = windowScene.delegate as? SceneDelegate, let window = delegate.window else { return nil }
+                   return window
+        }
+        
+        guard let window = UIApplication.shared.delegate?.window else { return nil }
+        return window
     }
     
     func hideKeyboardWhenTappedAround() {
