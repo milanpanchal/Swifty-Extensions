95464cdaf36db4839e03ea33594b0d96799bbd2a
Milan Panchal <sam07it22@gmail.com>
2020-11-02 17:04:33 +0530

    UINavigationItem title and subTitle

---
+++ Swifty-Extensions/Extensions/UIKit/UINavigationItem.swift
@@ -0,0 +1,35 @@
+//
+//  UINavigationItem.swift
+//  Swifty-Extensions
+//
+//  Created by Milan Panchal on 02/11/20.
+//  Copyright © 2020 Jeenal Infotech. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationItem {
+    
+    /// Set Navigation title and subtitle
+    func setTitle(_ title: String, subtitle: String) {
+        let appearance          = UINavigationBar.appearance()
+        let textColor           = appearance.titleTextAttributes?[NSAttributedString.Key.foregroundColor] as? UIColor ?? .black
+        
+        let titleLabel          = UILabel()
+        titleLabel.text         = title
+        titleLabel.font         = .preferredFont(forTextStyle: UIFont.TextStyle.headline)
+        titleLabel.textColor    = textColor
+        
+        let subtitleLabel       = UILabel()
+        subtitleLabel.text      = subtitle
+        subtitleLabel.font      = .preferredFont(forTextStyle: UIFont.TextStyle.subheadline)
+        subtitleLabel.textColor = textColor.withAlphaComponent(0.75)
+        
+        let stackView           = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        stackView.distribution  = .equalCentering
+        stackView.alignment     = .center
+        stackView.axis          = .vertical
+        
+        self.titleView          = stackView
+    }
+}
